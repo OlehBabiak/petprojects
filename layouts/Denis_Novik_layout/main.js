908fed8cb5666f4a1a9bca6495a3d2fd5a0a19d1
OlehBabiak <oleg_bob@ukr.net>
2021-01-31 22:29:27 +0200

    ghj

---
+++ layouts/Denis_Novik_layout/main.js
@@ -0,0 +1,7 @@
+document.querySelector('.button_mob').onclick = function () {
+    document.querySelector('.header').classList.toggle('active')
+}
+
+document.querySelector('.closer').onclick = function () {
+    document.querySelector('.header').classList.toggle('active');  
+}
